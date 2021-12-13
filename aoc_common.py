924fd6e5b80abd67c990cfa78ea4187fd4b1cf79
Bill Wu <hi@bw5.io>
2021-12-13 07:27:18 +0000

    AOC Common update - fileToMap

--- aoc_common.py
+++ aoc_common.py
@@ -1,9 +1,21 @@
-def fileToArray(filename):
+def fileToArray(filename, returnInt=False):
     output=[]
     file=open(filename)
     while True:
         thisline = file.readline().strip()
         if not thisline:
             break
+        if returnInt==True:
+            thisline=int(thisline)
         output.append(thisline)
+    return output
+
+def fileToMap(filename, sep, returnInt=False):
+    input=fileToArray(filename)
+    output=[]
+    for i in input:
+        line=i.split(sep)
+        if returnInt==True:
+            line=[int(element) for element in line]
+        output.append(line)
     return output
