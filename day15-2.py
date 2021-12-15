7eb5f9b9b6a75de5632ff6835b1f749dd4d9a116
Bill Wu <hi@bw5.io>
2021-12-15 20:33:55 +0000

    Day 15 Part 2

--- day15-2.py
+++ day15-2.py
@@ -1,11 +1,29 @@
 from aoc_common import fileToMap
 from copy import deepcopy
+from datetime import datetime
+start = datetime.now()
+def findShortest(obj, scMap, st=[]):
+    st.append([0,0,0])
+    while st:
+        current = st.pop(0)
+        x,y=current[0],current[1]
+        fourDirections=[(1,0),(0,1),(-1,0),(0,-1)]
+        for i,j in fourDirections:
+            if i+x in range(len(scMap)) and j+y in range(len(scMap)):
+                length=scMap[x][y]+obj[i+x][j+y]
+                if length<scMap[i+x][j+y] or scMap[i+x][j+y]==-1:
+                    scMap[i+x][j+y]=length
+                    st.append([i+x,j+y,length])
+        st.sort(key=lambda x: x[2])
+    return scMap[-1][-1]
+    
+    
 
-ogMap=fileToMap("test15.txt", '',True)
+ogMap=fileToMap("day15.txt", '',True)
 # print(ogMap)
 xMap=deepcopy(ogMap)
-for x in xMap:
-    print("".join([str(i) for i in x]))
+# for x in xMap:
+#     print("".join([str(i) for i in x]))
 
 for i in range(4):
     for x in range(len(ogMap)):
@@ -23,8 +41,9 @@ for i in range(4):
             newLine.append(xMap[x][y])
         newMap.append(newLine)
     yMap+=newMap
-
-
+# for x in yMap:
+#     print("".join([str(i) for i in x]))
 smap=[[-1 for _ in range(len(yMap[1]))] for _ in range(len(yMap))]
 smap[0][0]=0
-x,y=0,0
+print(findShortest(yMap,smap))
+print(f"Elapsed Time: {datetime.now()-start}")
